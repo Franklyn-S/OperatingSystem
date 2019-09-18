e9fd56aec70963f0abd8e6d8c9b50ab7b5eae711
Franklyn Seabra <franklyn.seabra@gmail.com>
2019-09-18 11:07:51 -0300

    1 questao feita

--- 4 - Proccess Synchronization/Questao 1/Consumer.java
+++ 4 - Proccess Synchronization/Questao 1/Consumer.java
@@ -5,15 +5,16 @@ public class Consumer implements Runnable {
     private final boolean even;
 
     public Consumer(boolean even, Dropbox dropbox) {
+        
         this.even = even;
         this.dropbox = dropbox;
     }
 
-    public void run() {
+    public synchronized void run() {
         Random random = new Random();
         while (true) {
-            dropbox.take(even);
             try {
+                dropbox.take(even);
                 Thread.sleep(random.nextInt(100));
             } catch (InterruptedException e) { }
         }
--- 4 - Proccess Synchronization/Questao 1/Dropbox.java
+++ 4 - Proccess Synchronization/Questao 1/Dropbox.java
@@ -1,14 +1,41 @@
 public class Dropbox {
+
     private int number;
     private boolean evenNumber = false;
-    public int take(final boolean even) {
-        System.out.format("%s CONSUMIDOR obtem %d.%n", even ? "PAR" : "IMPAR", number);
+    private boolean avaiable = true;
+    private boolean canProduce = true;
+    
+    
+    private boolean checkNumberParity() {
+        return this.number % 2 == 0;
+    }
+
+    private synchronized void changeAvaibility(boolean availability) {
+        avaiable = availability;
+    }
+
+    public synchronized int take(final boolean even) throws InterruptedException {
+
+        if( avaiable && checkNumberParity() == even) {
+            System.out.format("%s CONSUMIDOR obtem %d.%n", even ? "PAR" : "IMPAR", number);
+            changeAvaibility(false);
+            canProduce = true;
+        } else {
+            wait();
+        }
+
         return number;
     }
 
-    public void put(int number) {
-        this.number = number;
-        evenNumber = number % 2 == 0;
-        System.out.println("PRODUTOR gera %d.%n", number);
+    public synchronized void put(int number) throws InterruptedException {
+        if (canProduce) {
+            this.number = number;
+            evenNumber = number % 2 == 0;
+            System.out.format("PRODUTOR gera %d.%n", number);
+
+            changeAvaibility(true);
+            canProduce = false;
+            notifyAll();
+        }
     }
 }
