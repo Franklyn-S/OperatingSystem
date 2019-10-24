0841ef726fc31b62976a2fe3350a6a14c3e82f2e
Franklyn Seabra <franklynseabra12@hotmail.com>
2019-10-23 23:48:04 -0300

    correction

--- 0-Scripts/backup.sh
+++ 0-Scripts/backup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 DATA=`date +%Y-%m-%d-%H.%M`
 ONEDAYAGO=`(date --date '1 day ago' --rfc-3339=seconds)`
-DIR_RELATIVO="${0%/*}"
-DIR_CHAMADA="${PWD}"
-SCRIPT_PATH=$DIR_CHAMADA/$DIR_RELATIVO
-#find $SCRIPT_PATH -mtime -1
-tar -czP --newer-mtime="$ONEDAYAGO" --recursion -p --file=$DATA.tar $DIR_CHAMADA
+SCRIPT_PATH="${PWD}"
+tar -czP --newer-mtime="$ONEDAYAGO" --recursion -p --file=$DATA.tar $SCRIPT_PATH
 echo "Done!"
